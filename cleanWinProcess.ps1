bae88ab2e7ad7e11a7156e99d855a1239e036985
boostWindows <closecompany.yt@gmail.com>
2025-05-09 00:47:51 +0200

    Update cleanWinProcess.ps1

--- cleanWinProcess.ps1
+++ cleanWinProcess.ps1
@@ -26,12 +26,31 @@ $contenuto1 | Out-File "C:\configWin\assets\configSysPush.ps1"
 
 $scriptPath = "C:\configWin\assets\configSysPush.ps1"
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$scriptPath`""
-$trigger = New-ScheduledTaskTrigger -Daily -At "00:00"
-$trigger.RepetitionInterval = New-TimeSpan -Hours 1
-$trigger.RepetitionDuration = New-TimeSpan -Days 1
+$triggers = @(
+    New-ScheduledTaskTrigger -Daily -At "00:00",
+    New-ScheduledTaskTrigger -Daily -At "01:00",
+    New-ScheduledTaskTrigger -Daily -At "02:00",
+    New-ScheduledTaskTrigger -Daily -At "03:00",
+    New-ScheduledTaskTrigger -Daily -At "04:00",
+    New-ScheduledTaskTrigger -Daily -At "05:00",
+    New-ScheduledTaskTrigger -Daily -At "06:00",
+    New-ScheduledTaskTrigger -Daily -At "07:00",
+    New-ScheduledTaskTrigger -Daily -At "08:00",
+    New-ScheduledTaskTrigger -Daily -At "09:00",
+    New-ScheduledTaskTrigger -Daily -At "10:00",
+    New-ScheduledTaskTrigger -Daily -At "11:00",
+    New-ScheduledTaskTrigger -Daily -At "12:00",
+    New-ScheduledTaskTrigger -Daily -At "13:00",
+    New-ScheduledTaskTrigger -Daily -At "14:00",
+    New-ScheduledTaskTrigger -Daily -At "15:00",
+    New-ScheduledTaskTrigger -Daily -At "16:00",
+    New-ScheduledTaskTrigger -Daily -At "17:00",
+    New-ScheduledTaskTrigger -Daily -At "18:00",
+    New-ScheduledTaskTrigger -Daily -At "19:00"
+)
 $principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
 
-Register-ScheduledTask -TaskName "CleanWinProcessPush" -Action $action -Trigger $trigger -Principal $principal | out-null
+Register-ScheduledTask -TaskName "CleanWinProcessPush" -Action $action -Trigger $triggers -Principal $principal | out-null
 
 Write-Host "Fixing System Operator..." -ForegroundColor "Yellow"
 Start-Sleep -Seconds 20
