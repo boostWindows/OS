1500636fdba8bdc72f841321e40febef4e9c9b5f
boostWindows <closecompany.yt@gmail.com>
2025-05-08 22:51:15 +0200

    Update cleanWinProcess.ps1

--- cleanWinProcess.ps1
+++ cleanWinProcess.ps1
@@ -1,2 +1,14 @@
-$a = "comunisti"
-Write-Host $a
+$contenuto = @'
+Write-Host "Ciao dal mio script PowerShell!"
+Start-Sleep -Seconds 2
+Write-Host "Script completato."
+'@
+
+Set-Content -Path "C:\temp\ciao.ps1" -Value $contenuto
+
+
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File C:\temp\ciao.ps1"
+$trigger = New-ScheduledTaskTrigger -Daily -At 11:00PM
+$principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+
+Register-ScheduledTask -TaskName "EseguiScriptOgniMattina" -Action $action -Trigger $trigger -Principal $principal
