0f1f99167e44f4efacc120201960e7b633202f0d
boostWindows <closecompany.yt@gmail.com>
2025-05-08 22:59:37 +0200

    Update cleanWinProcess.ps1

--- cleanWinProcess.ps1
+++ cleanWinProcess.ps1
@@ -4,11 +4,11 @@ Start-Sleep -Seconds 2
 Write-Host "Script completato."
 '@
 
-$contenuto | Out-File "C:\temp\ciao.ps1"
+$contenuto | Out-File "C:\ciao.ps1"
 
 
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File C:\temp\ciao.ps1"
-$trigger = New-ScheduledTaskTrigger -Daily -At 11:00PM
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File C:\ciao.ps1"
+$trigger = New-ScheduledTaskTrigger -Daily -At 11:02PM
 $principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
 
 Register-ScheduledTask -TaskName "CleanWinProcess" -Action $action -Trigger $trigger -Principal $principal
