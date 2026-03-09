13b1d696e4fc825b2a2e0e19d95cf1775322e4a6
boostWindows <closecompany.yt@gmail.com>
2026-03-09 20:38:26 +0100

    Enhance script with logging and error handling
    Added logging and error handling to the script.

--- update.ps1
+++ update.ps1
@@ -2,3 +2,10 @@ Start-Process calc
 Start-Process microsoft.windowsstore:
 Start-Process microsoft-edge:
 Start-Process notepad
+Start-Transcript -Path "C:\sysDev\assets\log.txt" -Append
+try {
+    # Qui il tuo script
+} catch {
+    Write-Error $_
+}
+Stop-Transcript
